ecb4046f5d226201bf320474c5884fb441582999
Jesse Squires <jesse.d.squires@gmail.com>
2013-06-06 16:07:39 -0400

    Initial Commit

---
+++ objc-sorts/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  objc-sorts
+//
+//  Created by Jesse Squires on 6/6/13.
+//  Copyright (c) 2013 Hexed Bits. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
