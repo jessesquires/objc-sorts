89da99cd008efbc2c553d93fced05abc682f235a
Jesse Squires <jesse.d.squires@gmail.com>
2013-06-06 16:32:17 -0400

    ignore. readme.

--- objc-sorts/main.m
+++ objc-sorts/main.m
@@ -10,12 +10,8 @@
 
 int main(int argc, const char * argv[])
 {
-
     @autoreleasepool {
-        
-        // insert code here...
         NSLog(@"Hello, World!");
-        
     }
     return 0;
 }
